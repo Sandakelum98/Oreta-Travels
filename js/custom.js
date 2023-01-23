98c95ebf3fa40a03bb2ef23cb17c3fd8f0e79830
Sanjaya Sandakelum <sanjaya.d@ts.lk>
2023-01-23 15:31:54 +0530

    payments page

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,12 @@
 (function ($) {
     "use strict";
     $(document).ready(function () {
+
         $('.mobile-menu-toggle').on('click', function () {
             $('.student-sidebar').addClass('show-sidebar');
             $('body').toggleClass('menu-has-opened');
         });
+
         $('.schedule-tabs .schedule-tab').on('click', function () {
             $('.time-table').hide();
             var dataId = $(this).data('id');
@@ -12,6 +14,7 @@
             $('.schedule-tabs .schedule-tab').removeClass('active');
             $(this).addClass('active');
         });
+
         $('.testimonial-slider').owlCarousel({
             loop: true,
             margin: 0,
@@ -48,6 +51,20 @@
         $('.td-row .td-title').on('click', function () {
             $(this).next('.td-desc').slideToggle();
         });
+
+
+        // header slide menu
+        $('.slide_menu_open').on('click', function () {
+            $('.slide-menu-hide').animate({
+                'right': '0px'
+            });
+        });
+        $('.slide_menu_close').on('click', function () {
+            $('.slide-menu-hide').animate({
+                'right': '-540px'
+            });
+        });
+
 
     });
 })(jQuery);
